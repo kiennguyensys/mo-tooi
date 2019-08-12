b90c2c2c561a0c961a5fa422d768426350172986
ADMIN <admin@QBs-MacBook-Pro.local>
2019-08-12 11:35:39 +0700

    modify index.js

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,26 @@
 import { Component } from 'react';
 import axios from 'axios'
-import {store} from '../redux/store';
-import {Provider} from 'react-redux';
+import {connect} from 'react-redux';
 import { Container, Row, Col } from 'react-bootstrap';
-class App extends Component {
 
-    static async getInitialProps() {
+class Index extends Component {
+
+    static async getInitialProps({store, isServer, pathname, query}) {
         let response = await axios.get('http://localhost:3000/api/posts');
         return { posts: response.data };
     }
 
     render() {
         return (
-            <Provider store={store}>
-                <Container fluid>
-                    <Row className='flex-grow-1'>
-                        <Col className="bg-primary"></Col>
-                        <Col className="bg-info "></Col>
-                        <Col className="bg-danger"></Col>
-                    </Row>
-                </Container>
-            </Provider>
+            <Container fluid>
+                <Row className='flex-grow-1'>
+                    <Col className="bg-primary"></Col>
+                    <Col className="bg-info "></Col>
+                    <Col className="bg-danger"></Col>
+                </Row>
+            </Container>
         );
     };
 }
 
-export default App;
+export default connect(state => state)(Index);
