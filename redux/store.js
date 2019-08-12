c795b553a156fcba77e1f17c3ed0361723691d31
Neo <quangman1404@gmail.com>
2019-08-12 10:52:03 +0700

    Update store.js

--- redux/store.js
+++ redux/store.js
@@ -3,11 +3,8 @@ import thunk from 'redux-thunk';
 import reducer from './reducers/reducers.js';
 
 // in this file we are initializing the redux store by passing initial state and instance of reducer, we are applying thunk middleware to support async data flow.
-<<<<<<< HEAD
-=======
 const composeEnhancers = typeof window != 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
->>>>>>> master
 export const store = (initialState = {}) => {
   return createStore(
       reducer,
